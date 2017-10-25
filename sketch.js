89ce5bbcabaa575f78887f538f537792eb4ed00d
Neous1 <Neous1@users.noreply.github.com>
2017-10-25 11:15:59 -0400

    part 2 coding challenge add a textarea on front end to capture text
    add typing function to capture and compare to the json file to grab the score
    add up the score
    
    display data on html side

--- sketch.js
+++ sketch.js
@@ -1,12 +1,40 @@
 var table;
 
 function preload(){
+    afinn = loadJSON("afinn111.json");
 }
 
 function setup() {
     noCanvas();
+    console.log(afinn);
 
+    var txt = select("#txt");//, [container])
+    txt.input(typing);
 
+    function typing(){
+        var textInput = txt.value();
+        var words = textInput.split(/\W/);// this regext looks for all non letters
+        console.log(words);
+        var scoredwords = [];
+        totalScore = 0;
+        for(var i =0; i< words.length; i++){
+            var word = words[i].toLowerCase();
+            if(afinn.hasOwnProperty(word)){
+                var score = afinn[word];
+                console.log(word, score);
+                totalScore += Number(afinn[word]);
+                scoredwords.push(word + ": " + score + ' ')
+            }
+        }
+        var scoreP = select("#score");
+        scoreP.html("score:" + totalScore);
+        var comp = select("#comparative");
+        comp.html("comparative:" + totalScore / words.length);
+        var wordlist = select("#wordlist");
+        wordlist.html(scoredwords);
+        // console.log(txt.value());
+    }
+}
 
 function draw() {
 
